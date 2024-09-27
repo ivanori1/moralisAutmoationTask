a22e82dd1bd56ea897e17b223c701c358f0a6fe5
ivanori1 <coric.ivan@yandex.com>
2024-09-27 22:25:38 +0200

    update env file so it overwrite value if new is store, for example node key is delete and new one is create, it is overwrite

--- utils/env.ts
+++ utils/env.ts
@@ -13,14 +13,27 @@ export const getEnvVar = (key: string): string => {
 }
 
 export const setEnvVar = (key: string, value: string): void => {
-    const envPath = resolve(__dirname, '../.env')
-    const existingEnvFile = readFileSync(envPath, 'utf-8')
-    const keyExist = existingEnvFile.split('\n').some((line) => line.startsWith(`${key}=`) && line.split('=')[1].trim())
-    if (keyExist) {
-        console.log(`${key} is already set`)
-        return
+    const envPath = resolve(__dirname, '../.env');
+    const envFileContent = readFileSync(envPath, 'utf-8');
+  
+    // Update existing key or append a new key-value pair
+    const newEnvFileContent = envFileContent
+      .split('\n')
+      .map(line => {
+        if (line.startsWith(`${key}=`)) {
+          return `${key}=${value}`;
+        }
+        return line;
+      })
+      .join('\n');
+  
+    if (!envFileContent.includes(`${key}=`)) {
+      // If key doesn't exist, append it
+      writeFileSync(envPath, `${envFileContent.trim()}\n${key}=${value}\n`);
+      console.log(`${key} added to .env file.`);
+    } else {
+      // Overwrite the existing key
+      writeFileSync(envPath, newEnvFileContent);
+      console.log(`${key} updated in .env file.`);
     }
-    const evnFile = `${key}=${value}\n`
-    writeFileSync(envPath, evnFile, {flag:'a'})
-    console.log(`${key} is written to .env file`)
-}
+  };
